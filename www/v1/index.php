384c180c6e4d8e183c610b62a0ffce0c2f00e637
ryhoneyman <ryhoneyman@gmail.com>
2024-02-29 14:32:09 +0000

    adjusted paths, debugging

--- www/v1/index.php
+++ www/v1/index.php
@@ -3,12 +3,13 @@ include_once 'yaqds-api-init.php';
 include_once 'local/main.class.php';
 
 $main = new Main(array(
-   'debugLevel'     => 0,
+   'debugLevel'     => 9,
+   'debugBuffer'    => true,
    'errorReporting' => false,
    'sessionStart'   => false,
    'memoryLimit'    => null,
    'sendHeaders'    => false,
-   'database'       => false,
+   'database'       => 'prepare',
    'input'          => false,
    'html'           => false,
    'adminlte'       => false,
@@ -16,69 +17,48 @@ $main = new Main(array(
 
 spl_autoload_register('autoLoader');
 
-$starttime = microtime(true);
-
-$main->buildClass('apicore','ApiCore',null,'local/apicore.class.php');
+$main->buildClass('apicore','ApiCore',$main->db(),'local/apicore.class.php');
 $main->buildClass('token','Token',null,'local/token.class.php');
 $main->buildClass('request','Request',null,'local/request.class.php');
 $main->buildClass('response','Response',null,'local/response.class.php');
 $main->buildClass('router','Router',null,'local/router.class.php');
 
-$debuglevel   = 0;                    // global debug level 0(none) - 9(full)
-$debugbuffer  = true;                 // buffer debug information and display at the end (prevents header interference)
-$ratelimit    = true;                 // rate limit resets to the API, reduces overall speed - offers overload protection
-$basedir      = '/opt/epic/api/v2';   // base directory for the API
-$durationLast = 0;                    // used to calculate debug event durations
+$main->prepareDatabase(APP_CONFIGDIR.'/db-yaqds.conf','yaqds');
 
-$debug    = new Debug($debuglevel,$debugbuffer);
-$cipher   = new Cipher($debug);
-$dbh      = new MySQL($debug);
-$apicore  = new APICore($debug,$dbh);
-$apiauth  = new APIAuth($debug);
-$request  = new Request($debug);
-$response = new Response($debug);
-$router   = new Router($debug);
-$token    = new Token($debug);
+$apicore  = $main->obj('apicore');
+$router   = $main->obj('router');
+$request  = $main->obj('request');
+$response = $main->obj('response');
+$token    = $main->obj('token');
 
-showDuration("Start, classes built"); 
+$apiSettings = json_decode(file_get_contents(V1_CONFIGDIR.'/api.settings.json'),true);
+
+$main->debug->traceDuration("Start, classes built"); 
+
+$rateLimit = $apiSettings['rateLimit'];
 
 // Set directories for file access
-$apicore->tokenDir   = $basedir.'/tokens';
-$router->endpointDir = $basedir.'/etc/endpoints';
-
-// Set database connect parameters that come from api.conf
-// but defer connecting until we need the database to improve performance
-$dbh->setParameters(array('host'     => $dbhost, 
-                          'dbname'   => $dbname, 
-                          'username' => $cipher->decode($dbuser), 
-                          'password' => $cipher->decode($dbpass)));
-
-// libraries to make available to controllers
-$libs = array(
-   'apicore' => $apicore,
-   'apiauth' => $apiauth,
-   'dbh'     => $dbh,
-   'debug'   => $debug,
-);
+$apicore->tokenDir   = V1_TOKENDIR;
+$router->endpointDir = V1_CONFIGDIR.'/endpoints';
 
 // Debug user request object
 //$debug->trace(9,json_encode($request));
 
 // If the request wasn't for authentication and we have a Authorization Basic header we have to process it
-if (!$apiauth->isAuthRequest($router->categoryName) && preg_match('/^basic\s+/i',$request->auth)) {
-   $authController = new AuthController($debug,$libs);
+if (!$apicore->isAuthRequest($router->categoryName) && preg_match('/^basic\s+/i',$request->auth)) {
+   $authController = new AuthController($main->debug,$main);
    $authController->authBasic($request);
 }
 
 // If we were supplied a token, prefetch and store token data for global use
 if ($request->token) {
    $token->mapData($apicore->getTokenData($request->token));
-   showDuration("token mapped"); 
+   $main->debug->traceDuration("token mapped"); 
 
-   if ($ratelimit && $token->valid) {
+   if ($rateLimit && $token->valid) {
       // Borrow a use unit from the pool for our token
       $result = $apicore->addTokenCounter($token);
-      showDuration("token counter adjusted up"); 
+      $main->debug->traceDuration("token counter adjusted up"); 
    }
 }
 
@@ -86,21 +66,21 @@ if ($request->token) {
 //$debug->trace(0,json_encode($token));
 
 // Check if this token is currently exceeds its limits
-if (!$ratelimit || !$token->limitExceeds) {
+if (!$rateLimit || !$token->limitExceeds) {
    // Match the request path with known endpoints and determine if it exists
    if (!$router->processRequestPath($request)) { 
       $response->setStatus(404,'Resource Not Found');
    }
    else {
       // Check user token to see if we are authorized to use the API call
-      $allowed = checkToken($token,$request,$response,$router,$libs);
-      showDuration("token checked"); 
+      $allowed = checkToken($main);
+      $main->debug->traceDuration("token checked"); 
 
       if ($allowed) {
          // Execute the API call, a false return indicates a non-user error
          // At the moment the return isn't used, but it could be in the future
-         $result = apiMain($request,$response,$router,$libs);
-         showDuration("API endpoint finish"); 
+         $result = apiMain($main);
+         $main->debug->traceDuration("API endpoint finish"); 
       }
    }
 }
@@ -109,12 +89,12 @@ else {
 }
 
 // Add rate limit headers if limiting is on
-if ($ratelimit && $token->rateLimit) {
+if ($rateLimit && $token->rateLimit) {
    $rateRemaining = ($token->rateLimit > $token->rateCount) ? $token->rateLimit - $token->rateCount : 0;
    $response->addCustomHeader('X-RateLimit-Limit: '.$token->rateLimit);
    $response->addCustomHeader('X-RateLimit-Remaining: '.$rateRemaining);
    $response->addCustomHeader('X-RateLimit-Reset: '.date('r',$token->rateExpires));
-   showDuration("ratelimit header set");
+   $main->debug->traceDuration("rateLimit header set");
 }
 
 // set HTTP protocol response
@@ -122,36 +102,35 @@ $response->setProtocol($request->protocol);
 
 // Output response to client
 $response->sendHeader($token);
-showDuration("header sent"); 
+$main->debug->traceDuration("header sent"); 
 $response->sendBody();
-showDuration("body sent"); 
+$main->debug->traceDuration("body sent"); 
 
 // Return our use unit to the pool for our token
-if ($ratelimit && $token->valid) {
+if ($rateLimit && $token->valid) {
    $result = $apicore->removeTokenCounter($token);
-   showDuration("token counter adjusted down"); 
+   $main->debug->traceDuration("token counter adjusted down"); 
 }
-
-$finishtime  = microtime(true);
-$elapsedtime = $finishtime - $starttime;
 
 // Log user request and response to database
 $apicore->logApiRequestFile($request,$response,$elapsedtime);
-showDuration("request logged"); 
+$main->debug->traceDuration("request logged"); 
 
-$debug->trace(9,sprintf("total duration: %1.6f",$elapsedtime));
+$main->debug->traceDuration("total time",$main->startMs);
 
 // If debug is enabled, output debug information
-if ($debuglevel > 0) { print $debug->logOutput(); }
+if ($debuglevel > 0) { print $main->debug->logOutput(); }
 
 // END MAIN /-----------------------------------------------------------------------
 
 ?>
 <?php
 
-function apiMain($request, $response, $router, $libs)
+function apiMain($main)
 {
-   $debug = $libs['debug'];
+   $request  = $main->obj('request');
+   $response = $main->obj('response');
+   $router   = $main->obj('router');
 
    // Get controller and function from router
    $controllerName = $router->controllerName;
@@ -163,7 +142,7 @@ function apiMain($request, $response, $router, $libs)
       return false;
    }
 
-   $controller = new $controllerName($debug,$libs);
+   $controller = new $controllerName($main->debug,$main);
 
    // Controller could not be initialized
    if (!$controller) {
@@ -171,19 +150,22 @@ function apiMain($request, $response, $router, $libs)
       return false;
    }
 
-   // Check to see if the class method (endpoint function) exists in the controller class
-   if (!method_exists($controller,$functionName)) {
-      $response->setStatus(405,'Function Not Implemented');
-      return false;
-   }
+   // Call controller function, only if it indicates it's ready
+   if ($controller->ready) { 
+      // Check to see if the class method (endpoint function) exists in the controller class
+      if (!method_exists($controller,$functionName)) {
+         $response->setStatus(405,'Function Not Implemented');
+         return false;
+      }
 
-   // Make function call to the controller
-   $cfResult = $controller->$functionName($request);
+      // Make function call to the controller
+      $cfResult = $controller->$functionName($request);
 
-   // Something went wrong in the controller method
-   if (!$cfResult) {
-      $response->setStatus(500,'Controller Function Error');
-      return false;
+      // Something went wrong in the controller method
+      if (!$cfResult) {
+         $response->setStatus(500,'Controller Function Error');
+         return false;
+      }
    }
 
    // set initial status from controller
@@ -203,7 +185,7 @@ function apiMain($request, $response, $router, $libs)
       return false;
    }
 
-   $view = new $viewName($debug);
+   $view = new $viewName($main->debug);
 
    // View could not be initialized
    if (!$view) {
@@ -223,16 +205,25 @@ function apiMain($request, $response, $router, $libs)
    $response->setContentType($view->contentType);
    $response->setContentBody($view->contentBody);
 
+   // Controller initialized, but marked itself not ready.  
+   // We wait until here because we need the headers/content (which may contain error messages) to render
+   if (!$controller->ready) { return false; }
+
    return true;
 }
 
-function checkToken($token, $request, $response, $router, $libs)
+function checkToken($main)
 {
-   $apiauth = $libs['apiauth'];
-   $debug   = $libs['debug'];
+   $apicore  = $main->obj('apicore');
+   $token    = $main->obj('token');
+   $request  = $main->obj('request');
+   $response = $main->obj('response');
+   $router   = $main->obj('router');
 
    // If the request was for authentication, there's no need to check the token
-   if ($apiauth->isAuthRequest($router->categoryName)) { return true; }
+   if ($apicore->isAuthRequest($router->categoryName)) { return true; }
+
+   //$main->debug->trace(9,"token:".json_encode($token,JSON_UNESCAPED_SLASHES));
 
    // If user supplied a token, validate it's properites
    if ($request->token) {
@@ -244,7 +235,7 @@ function checkToken($token, $request, $response, $router, $libs)
          $response->setStatus(401,'Token Expired');
          return false;
       }
-      else if (!$apiauth->authorize($token->accessList,$router->controllerName,$router->functionName,$request->method)) {
+      else if (!$apicore->authorize($token->accessList,$router->controllerName,$router->functionName,$request->method)) {
          $response->setStatus(403,'Insufficent Privilege');
          return false;
       }
@@ -259,47 +250,31 @@ function checkToken($token, $request, $response, $router, $libs)
 
 function autoLoader($classname)
 {
-   global $debug;
+   global $main;
 
    $lcname = strtolower($classname);
 
-   $debug->trace(9,'Looking for class '.$classname);
+   $main->debug->trace(9,'Looking for class '.$classname);
 
    if (preg_match('/[a-z0-9]+(model|view|controller)$/i',$classname,$match)) {
       $type = strtolower($match[1]);
-      $file .= __DIR__."/{$type}s/$lcname.class.php";
+      $file .= V1_BASEDIR."/{$type}s/$lcname.class.php";
 
-      $debug->trace(9,'Trying to load '.$type.' file: '.$file);
+      $main->debug->trace(9,'Trying to load '.$type.' file: '.$file);
 
       if (file_exists($file)) {
          $return = (!@include_once($file)) ? false : true;
-         $debug->trace(9,'File found: '.$file.' [success:'.$return.']');
+         $main->debug->trace(9,'File found: '.$file.' [success:'.$return.']');
          return $return;
       }
       else {
-         $debug->trace(9,'File not found: '.$file);
+         $main->debug->trace(9,'File not found: '.$file);
       }
    }
 
-   $debug->trace(9,'Class '.$classname.' not valid for autoload.');
+   $main->debug->trace(9,'Class '.$classname.' not valid for autoload.');
 
    return false;
 }
 
-function showDuration($label)
-{
-   global $debug, $durationLast;
-
-   if ($debug->level < 9) { return false; }
-
-   $now = microtime(true);
-
-   if (!$durationLast) { $durationLast = $now; }
-   
-   $debug->trace(9,sprintf("%s: %1.6f secs",$label,($now - $durationLast)));
-
-   $durationLast = $now;
-
-   return true;
-}
 ?>
