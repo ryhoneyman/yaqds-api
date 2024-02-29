768aaf902f03a6b81d43c32800d2327cce8f1acb
ryhoneyman <ryhoneyman@gmail.com>
2024-02-29 14:31:12 +0000

    updated init

--- deploy/yaqds-api-init.php
+++ deploy/yaqds-api-init.php
@@ -1,12 +1,13 @@
 <?php
 
 define('APP_BASEDIR','/opt/yaqds-api');
-define('V1_BASEDIR',APP_BASEDIR.'/v1');
+define('APP_CONFIGDIR',APP_BASEDIR.'/etc');      // global configurations
+define('V1_BASEDIR',APP_BASEDIR.'/core/v1');
 define('V1_LIBDIR',V1_BASEDIR.'/lib');
 define('V1_WEBDIR',V1_BASEDIR.'/www');
-define('V1_CONFIGDIR',V1_BASEDIR.'/etc');    // static configurations
-define('V1_VARDIR',V1_BASEDIR.'/var');       // dynamic file data
-define('V1_LOGDIR',V1_BASEDIR.'/log');       // logs
+define('V1_CONFIGDIR',V1_BASEDIR.'/etc');        // v1 static configurations
+define('V1_TOKENDIR',APP_BASEDIR.'/tokens/v1');  // v1 cached tokens
+define('V1_LOGDIR',APP_BASEDIR.'/log/v1');       // v1 logs
 
 set_include_path(get_include_path().PATH_SEPARATOR.V1_LIBDIR);
 
