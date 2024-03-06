3e863e8dab162ce9dddad531be3b12bbb1ed8c53
ryhoneyman <ryhoneyman@gmail.com>
2024-03-06 16:21:07 +0000

    include path update

--- deploy/yaqds-api-init.php
+++ deploy/yaqds-api-init.php
@@ -2,6 +2,7 @@
 
 define('APP_BASEDIR','/opt/yaqds-api');
 define('APP_CONFIGDIR',APP_BASEDIR.'/etc');      // global configurations
+define('APP_LWPLIBDIR',APP_BASEDIR.'/lwplib/lib');
 define('V1_BASEDIR',APP_BASEDIR.'/core/v1');
 define('V1_LIBDIR',V1_BASEDIR.'/lib');
 define('V1_WEBDIR',V1_BASEDIR.'/www');
@@ -9,6 +10,6 @@ define('V1_CONFIGDIR',V1_BASEDIR.'/etc');        // v1 static configurations
 define('V1_TOKENDIR',APP_BASEDIR.'/tokens/v1');  // v1 cached tokens
 define('V1_LOGDIR',APP_BASEDIR.'/log/v1');       // v1 logs
 
-set_include_path(get_include_path().PATH_SEPARATOR.V1_LIBDIR);
+set_include_path(get_include_path().PATH_SEPARATOR.V1_LIBDIR.PATH_SEPARATOR.APP_LWPLIBDIR);
 
 ?>
