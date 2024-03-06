70002b4a609e61798116c81998923bb15da5d536
ryhoneyman <ryhoneyman@gmail.com>
2024-03-06 16:20:28 +0000

    streamlined main to match base

--- core/v1/lib/main.class.php
+++ core/v1/lib/main.class.php
@@ -6,23 +6,4 @@ include_once 'mainbase.class.php';
 
 class Main extends MainBase
 {
-   public $constants      = null;
-   public $data           = null;
-
-   public function __construct($options = null)
-   {
-      parent::__construct($options);
-   }
-
-   public function initialize($options)
-   {
-      parent::initialize($options);
-
-      if ($options['constants']) {
-         if (!$this->buildClass('constants','Constants',null,'constants.class.php')) { exit; }
-         $this->constants = $this->obj('constants');
-      }
-
-      return true;
-   }
 }
