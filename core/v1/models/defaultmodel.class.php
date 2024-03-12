fd0bb72a454bfa31b37d35deb2322acca3d3162a
ryhoneyman <ryhoneyman@gmail.com>
2024-03-12 12:36:05 +0000

    modified ready behavior

--- core/v1/models/defaultmodel.class.php
+++ core/v1/models/defaultmodel.class.php
@@ -12,10 +12,7 @@ class DefaultModel extends LWPLib\Base
    {
       parent::__construct($debug);
 
-      if (!$main || !$main->obj('apicore')) { 
-         $this->error = 'Cannot locate main libraries';
-         $this->ready = false; 
-      }
+      if (!$main || !$main->obj('apicore')) { $this->notReady('Cannot locate main libraries'); return; }
 
       if ($this->ready) {
          $this->main    = $main;
@@ -24,11 +21,18 @@ class DefaultModel extends LWPLib\Base
          if ($this->needsDatabase) {
             $this->main->attachDatabase($this->needsDatabase);
 
-            if ($this->main->isDatabaseConnected($this->needsDatabase)) {
-               $this->error = 'Model cannot connect to database';
-               $this->ready = false;
-            }
+            if ($this->main->isDatabaseConnected($this->needsDatabase)) { $this->notReady('Model cannot connect to database'); return; }
          }
       }
+   }
+
+   public function notReady($error)
+   {
+      $this->debug(9,'Model signaliing not ready');
+
+      $this->ready = false;
+      $this->error = $error;
+
+      return true;
    }
 }
