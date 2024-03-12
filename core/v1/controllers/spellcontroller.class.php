e3e85d42df46a8d3984ec7753925abfb89abb181
ryhoneyman <ryhoneyman@gmail.com>
2024-03-12 12:35:13 +0000

    revamp for API based calls

--- core/v1/controllers/spellcontroller.class.php
+++ core/v1/controllers/spellcontroller.class.php
@@ -2,27 +2,36 @@
 
 class SpellController extends DefaultController
 {
+   protected $apiModel   = null;
    protected $spellModel = null;
-
+   
+   /**
+    * __construct
+    *
+    * @param  LWPLib\Debug|null $debug
+    * @param  Main|null $main
+    * @return void
+    */
    public function __construct($debug = null, $main = null)
    {
       parent::__construct($debug,$main);
       $this->debug(5,get_class($this).' class instantiated');
 
-      $main->attachDatabase('yaqds');
+      $this->apiModel = new ApiModel($debug,$main);
+
+      if (!$this->apiModel->ready) { $this->notReady($this->apiModel->error); return; }
 
       // The model provides all the data and methods to retrieve it; connect it and bring it online
       $this->spellModel = new SpellModel($debug,$main); 
 
       // If the model isn't ready we need to flag the controller as not ready and set status
-      if (!$this->spellModel->ready) { $this->notReady($this->spellModel->error); }
+      if (!$this->spellModel->ready) { $this->notReady($this->spellModel->error); return; }
    }
 
    public function getSpellList($request)
    {
       $this->debug(7,'method called');
 
-      $parameters = $request->parameters;
       $filterData = $request->filterData;
 
       $object = $filterData['object'];
