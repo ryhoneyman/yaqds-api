cf4258e8bae29a56c4ff98cbe0f9a8960dd95b94
ryhoneyman <ryhoneyman@gmail.com>
2025-04-21 17:50:01 +0000

    add customizable value separator

--- core/v1/lib/main.class.php
+++ core/v1/lib/main.class.php
@@ -6,11 +6,11 @@ include_once 'mainbase.class.php';
 
 class Main extends LWPLib\MainBase
 {
-    function replaceValues($string, $values)
+    function replaceValues($string, $values, $arraySeparator = '|')
     {
         if (!is_null($values) && is_array($values)) {
             $replace = array();
-            foreach ($values as $key => $value) { $replace['{{'.$key.'}}'] = ((is_array($value)) ? implode('|',array_filter(array_unique($value))) : ((is_bool($value)) ? json_encode($value) : $value)); }
+            foreach ($values as $key => $value) { $replace['{{'.$key.'}}'] = ((is_array($value)) ? implode($arraySeparator,array_filter(array_unique($value))) : ((is_bool($value)) ? json_encode($value) : $value)); }
 
             $string = str_replace(array_keys($replace),array_values($replace),$string);
         }
