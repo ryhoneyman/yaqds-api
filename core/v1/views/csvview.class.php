5e350ae9f880e2ec5ded3b677b225dd946a66318
ryhoneyman <ryhoneyman@gmail.com>
2025-09-11 17:53:42 +0000

    updated views

--- core/v1/views/csvview.class.php
+++ core/v1/views/csvview.class.php
@@ -1,14 +1,15 @@
 <?php
 
-class csvView extends View {
-   public function render($content) {
+class csvView extends DefaultView {
+   public function render($data = null) {
+      $method = '';
       header("Pragma: public");
       header("Cache-control: must-revalidate, post-check=0, pre-check=0");
       header("Content-type: application/octet-stream");
       header("Content-Disposition: attachment; filename=\"$method.csv\"");
 
       // ?????
-      echo $func->csv_put_contents($content);
+      //echo $func->csv_put_contents($data);
       return true;
    }
 }
--- core/v1/views/htmlview.class.php
+++ core/v1/views/htmlview.class.php
@@ -4,7 +4,7 @@ class HtmlView extends DefaultView {
   
    public $contentType = 'text/html; charset=utf8';
 
-   public function render($data) {
+   public function render($data = null) {
       $content = var_export($data,true);
       return $content;
    }
